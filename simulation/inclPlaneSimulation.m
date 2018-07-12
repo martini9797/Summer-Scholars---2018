1014eaeac419cda25a74d25581fad4301983c138
martini9797 <majkut.martin9797@gmail.com>
2018-07-12 16:49:46 -0400

    Animated box on inclined plane

--- simulation/inclPlaneSimulation.m
+++ simulation/inclPlaneSimulation.m
@@ -5,7 +5,7 @@ function inclPlaneSimulation(isFrictionVariable)
 %   plane or variable friction plane.
 %
 %   INPUTS:
-%       (optional) isFrictionVariable = bool = if true, will run simulation
+%       isFrictionVariable (optional) = bool = if true, will run simulation
 %           with a variable friction plane, where there are "patches" of 
 %           friction with different friction coefficients. Otherwise, will 
 %           run simulation with constant friction plane. Default = false
@@ -17,8 +17,7 @@ function inclPlaneSimulation(isFrictionVariable)
 %   Date: Jun. 14, 2018
 %
 
-% Clear workspace and add dynaics to path (a clear workspace is nice)
-close all; clc;
+% Add dynaics to path
 addpath('dynamicModelsForMatlabSimulation/');
 
 % Check for which dynamics model to run
@@ -28,7 +27,7 @@ end
 
 % Set initial conditions, parameters for dynamics and simulation
 tStart = 0;     % [s]
-tEnd   = 10;    % [s]
+tEnd   = 5;     % [s]
 nGrid  = 1000;
 tGrid  = linspace(tStart, tEnd, nGrid);
 
@@ -38,11 +37,10 @@ param.g     = 9.81;
 zInit = zeros(2,1);
 
 % Set friction model
-mus      = 0.5;
-muk      = 0.2;
-patchLen = 10;     %[m]
+mus      = 0.4;
+muk      = 0.1;
+patchLen = 10;  %[m]
 if isFrictionVariable
-    % Uncomment for on/off friction model 
     param.mus = @(x) mus * (mod(round(x/patchLen),2) - 1);
     param.muk = @(x) muk * (mod(round(x/patchLen),2) - 1);
 else
@@ -74,32 +72,85 @@ title('Velocity vs. Time');
 xlabel('Time [s]');
 ylabel('Velocity [m/s]');
 
-% Animate and Record the Simulation
-figure(62818); clf;
+% Animation settings
+boxH   = 0.05 * zGrid(1,end);   %[m]
+boxW   = 0.05 * zGrid(1,end);   %[m]
+boxCol = [48 15 0] ./ 255;  % rgb, Tufts brown for a brown box (:
+%dt     = tGrid(2) - tGrid(1);   %[s]
+
+% Animate Simulation
+hFig = figure(62818); clf;
 hold on;
 title('Animation of Sliding Box');
+daspect([1 1 1]);
+set(gca,'xtick',[],'ytick',[]);
 
+% Create rotation matrix to convert from local to global coordinate system
+r = [cos(param.theta) -sin(param.theta);
+     sin(param.theta)  cos(param.theta)];
+ 
 % Draw Inclined Plane
-planeThick = .01; %[m]
 xFlatStart = zGrid(1,1);
-r          = [cos(param.theta) -sin(param.theta); 
-              sin(param.theta)  cos(param.theta)];
 
 if isFrictionVariable
-    xFlatEnd = round(zGrid(1,end)/patchLen) * patchLen;
+    
+    % Determine the vertices of each patch along the plane
+    numPatches = round(zGrid(1,end)/patchLen);
+    xFlatEnd   = numPatches * patchLen;
+    xFlat      = xFlatStart:patchLen:xFlatEnd;
+    xMat       = [ xFlat(1:end-1); xFlat(2:end); xFlat(2:end); ...
+                   xFlat(1:end-1); xFlat(1:end-1)];
+    planeThick = 0.01 * xFlatEnd;      %[m]
+    yFlat      = planeThick * [0 0 -1 -1 0];
+    yMat       = repmat(yFlat,1,numPatches);
+    
+    % Reshape into coumn vectors to do math to rotate plane
+    xMat       = reshape(xMat,[],1);
+    yMat       = reshape(yMat,[],1);
+    planeCoor  = [xMat yMat] * r;
+    
+    % Reshape back into matrices that patch() accepts
+    xMat       = reshape(planeCoor(:,1),[],numPatches);
+    yMat       = reshape(planeCoor(:,2),[],numPatches);
+    patch(xMat,yMat,rand(numPatches,1));
+    axis( [ xMat(1,1) xMat(3,end) yMat(2,end) yMat(1,1)+boxH ] );
+
 else
-    xFlatEnd = zGrid(1,end);
-    xFlat    = [xFlatStart xFlatEnd xFlatEnd xFlatStart];
-    yFlat    = planeThick * [0 0 -1 -1];
+    
+    %Determine the vertices of the plane, rotate and draw it
+    xFlatEnd   = zGrid(1,end);
+    xFlat      = [xFlatStart xFlatEnd xFlatEnd xFlatStart]';
+    planeThick = 0.0025 * xFlatEnd;      %[m]
+    yFlat      = planeThick * [0 0 -1 -1]';
+    planeCoor  = [xFlat yFlat] * r;
+    patch(planeCoor(:,1),planeCoor(:,2),'k');
+    axis( [ planeCoor(1,1) planeCoor(2,1) planeCoor(end - 1,2) ...
+                 planeCoor(1,2)+boxH ] );
+
 end
 
-planeCoor = [xFlat; yFlat]' * r;
-planeCoor = planeCoor';
-axis([planeCoor(1,1) inf -inf planeCoor(2,end)]);
-daspect([1 1 1]);
-fill(planeCoor(1,:),planeCoor(2,:),'k');
+% Determine vertices of the box as it slides down the ramp
+xBox    = [ zGrid(1,:); zGrid(1,:); zGrid(1,:) + boxW; zGrid(1,:) + boxW ];
+yBox    = repmat([0; boxH; boxH; 0],1,size(zGrid,2));
+boxCoor = [reshape(xBox,[],1) reshape(yBox,[],1)] * r;
+xBox    = reshape(boxCoor(:,1),[],size(zGrid,2));
+yBox    = reshape(boxCoor(:,2),[],size(zGrid,2));
 
-
-
+% Animate
+startT = tic;
+box    = patch(xBox(:,1),yBox(:,1),boxCol);
+currT  = toc(startT);
+while currT < tGrid(end)
+    
+    delete(box);
+    
+    xNow = interp1(tGrid,xBox',currT,'linear');
+    yNow = interp1(tGrid,yBox',currT,'linear');
+    
+    box  = patch(xNow',yNow',boxCol);
+    drawnow;
+    
+    currT = toc(startT);
+end
 
 end
